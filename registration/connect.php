e6ee319bbeaf3cbd72a69a1912e2f1e74ad40149
Naman Agrawal <namanagrawal2097@gmail.com>
2018-04-22 00:59:29 +0530

    Connectioncodephp

---
+++ registration/connect.php
@@ -0,0 +1,36 @@
+<?php
+$user = 'root';
+$pass = '';
+$db = 'scholarshipdb';
+$db = new mysqli('localhost', $user, $pass, $db) or die();
+mysql_select_db("scholarshipdb")or die("no db found");
+if(isset($_POST['submit']))
+{
+    $name=$_POST['inputName'];
+    $DOB=$_POST['inputDOB'];
+    $Gen=$_POST['inputGender'];
+    $Mail=$_POST['inputEmail'];
+    $MOB=$_POST['inputMobile'];
+    $Cou=$_POST['inputCourse'];
+    $Year=$_POST['inputYear'];
+    $Roll=$_POST['inputRollNo'];
+    $URoll=$_POST['inputUpseeRollNo'];
+    $Cat=$_POST['inputCategory'];
+    $Rank=$_POST['inputRank'];
+    $CAdd=$_POST['inputCrrAddress'];
+    $PAdd=$_POST['inputPermAddress'];
+    $City=$_POST['inputCity'];
+    $State=$_POST['inputState'];
+    $Pin=$_POST['inputZip'];
+    $FN=$_POST['inputFatherName'];
+    $MN=$_POST['inputMotherName'];
+    $FO=$_POST['inputFatherOccupation'];
+    $MO=$_POST['inputMotherOccupation'];
+    $FI=$_POST['inputFatherIncome'];
+    $MI=$_POST['inputMotherIncome'];
+    $Inc=$_POST['inputIncome'];
+$query="insert inta data(Name,DOB,Gender,Email,Phone,Course,Year,Rollnumber,UPRollnumber,Category,Rank,CAddress,PAddress,City,State,Pincode,Fathername,Mothername,Fatherocc,Motherocc,FIncome,MIncome,Income)values('$name','$DOB','$Gen',' $Mail','$MOB','$Cou','$Year','$Roll',' $URoll','$Cat','$Rank','$CAdd','$PAdd','$City','$State','$Pin','$FN','$MN','$FO','$MO','$FI','$MI','$Inc')";
+
+}
+echo "great work";
+?>
